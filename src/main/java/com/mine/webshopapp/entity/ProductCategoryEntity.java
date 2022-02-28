5f9f8120308b57bf77e626879bb38e19bdfceacb
ShahinHajiyev <hshahin.t401@code.edu.az>
2022-02-28 18:44:44 +0100

    Entity classes to map with product and productcategory tables in the db are created and populated.

---
+++ src/main/java/com/mine/webshopapp/entity/ProductCategoryEntity.java
@@ -0,0 +1,27 @@
+package com.mine.webshopapp.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "productcategory")
+@Getter
+@Setter
+public class ProductCategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "productCategoryId")
+    private Long productCategoryId;
+
+    @Column(name = "categoryName")
+    private String categoryName;
+
+    @OneToMany(cascade= CascadeType.ALL, mappedBy = "productCategoryEntity")
+    private Set<ProductEntity> products;
+
+
+}
---
+++ src/main/java/com/mine/webshopapp/entity/ProductEntity.java
@@ -0,0 +1,60 @@
+package com.mine.webshopapp.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name="product")
+//@Data
+@Getter
+@Setter
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="productId")
+    private Long productId;
+
+    @Column(name="sku")
+    private String sku;
+
+    @Column(name="productName")
+    private String productName;
+
+    @Column(name="productDescription")
+    private String productDescription;
+
+    @Column(name="productUnitPrice")
+    private BigDecimal productUnitPrice;
+
+    @Column(name="imageURL")
+    private String imageURL;
+
+    @Column(name="active")
+    private boolean active;
+
+    @Column(name="piecesInStock")
+    private int piecesInStock;
+
+    @Column(name="creationDate")
+    @CreationTimestamp
+    private Date creationDate;
+
+    @Column(name="lastUpdate")                 //timestamp will  automatically be managed
+    @UpdateTimestamp
+    private Date lastUpdate;
+
+
+    @ManyToOne
+    @JoinColumn(name = "product_category_id")
+    private ProductCategoryEntity productCategoryEntity;
+
+
+}
