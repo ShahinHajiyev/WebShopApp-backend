c505b8ecba7a0ef86cb1fa8bb1ac5936b32c7503
ShahinHajiyev <hshahin.t401@code.edu.az>
2022-02-28 16:29:20 +0100

    Project is created Application.properties file filled with jdbc properties

---
+++ src/main/java/com/mine/webshopapp/WebshopappApplication.java
@@ -0,0 +1,13 @@
+package com.mine.webshopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mine/webshopapp/WebshopappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mine.webshopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
