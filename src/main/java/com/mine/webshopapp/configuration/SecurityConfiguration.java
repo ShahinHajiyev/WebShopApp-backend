69c1ba3c743f28e5403b86444923bd6b4e2e64c1
ShahinHajiyev <hshahin.t401@code.edu.az>
2022-03-18 21:33:57 +0100

    Security configuration added to secure orders api from unauthorized accesses. Used OKTA server to secure backend. application.properties modified with credentials of OKTA application account.

---
+++ src/main/java/com/mine/webshopapp/configuration/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.mine.webshopapp.configuration;
+
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // protect endpoint /api/orders
+        http.authorizeRequests()
+                .antMatchers("/api/orders/**")
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        // cors filters
+        http.cors();
+
+        //response message for code 401
+        Okta.configureResourceServer401ResponseBody(http);
+    }
+}
