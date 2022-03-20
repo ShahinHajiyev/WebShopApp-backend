ce121de757e72a3cd191cf6cfe3758daa852d119
ShahinHajiyev <hshahin.t401@code.edu.az>
2022-03-20 19:46:49 +0100

    openssl backend configuration added.

--- src/main/java/com/mine/webshopapp/dao/CountryRepository.java
+++ src/main/java/com/mine/webshopapp/dao/CountryRepository.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
-@CrossOrigin("http://localhost:4200")
+
 @RepositoryRestResource (collectionResourceRel = "countries", path = "countries")
 public interface CountryRepository extends JpaRepository<Country, Integer> {
 
--- src/main/java/com/mine/webshopapp/dao/ProductRepository.java
+++ src/main/java/com/mine/webshopapp/dao/ProductRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@CrossOrigin("http://localhost:4200")
+
 @RepositoryRestResource(collectionResourceRel = "products", path = "products")
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
